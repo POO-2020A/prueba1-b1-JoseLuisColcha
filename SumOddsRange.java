792586cee114f0f603e8d566acb9c99d969e7c8b
José Luis Colcha <jose.colcha@epn.edu.ec>
2020-07-06 11:56:59 -0500

    Update SumOddsRange.java

--- SumOddsRange.java
+++ SumOddsRange.java
@@ -27,12 +27,16 @@ Ejemplo de entrada/salida:
 class SumOddsRange {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
-    System.out.print("#1: ");
+    System.out.println("#1");
     int num1 = s.nextInt();
-    System.out.print("#2: ");
+    System.out.println("#2");
     int num2 = s.nextInt();
-    //start below
-    
-    
+    int sum = 0;
+    for (int i = num1; i <= num2; i++) {
+        if (i % 2 != 0) {
+            sum += i;
+          }
+      }
+      System.out.println(sum);   
   }
 }
